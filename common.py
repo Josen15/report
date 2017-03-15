274e1b6855d7c9ecb7919cf14fdc895aa00787b3
zhongsheng wang <zhongsheng.wang@okkuaixiu.com>
2017-03-15 17:33:37 +0800

    four

---
+++ common.py
@@ -0,0 +1,15 @@
+﻿#!/usr/bin/env python
+#coding:utf-8
+from csv import reader
+#读取csv文件
+def getCsv(file_name):
+	h=file(file_name,"r")
+	n=reader(h)
+	print n
+	list=[]
+	for x in n:
+		list.append(x)
+	a=list[1:]
+	return a
+getCsv(r'C:\\workspace\\n.csv')
+
